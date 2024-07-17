539b7ea45816a54bbc69f253fe8de03e768e24cc
reyhanehtpour <C@balt2019>
2024-07-17 12:09:34 -0700

    new design v3 with onboard

--- lib/util/design/helper_functions/video_player_screen.dart
+++ lib/util/design/helper_functions/video_player_screen.dart
@@ -1,70 +1,70 @@
-import 'package:flutter/material.dart';
-import 'package:video_player/video_player.dart';
-
-import '../color/app_colors.dart';
-
-
-class VideoPlayerScreen extends StatefulWidget {
-  const VideoPlayerScreen({super.key});
-
-  @override
-  _VideoPlayerScreenState createState() => _VideoPlayerScreenState();
-}
-
-class _VideoPlayerScreenState extends State<VideoPlayerScreen> {
-  late VideoPlayerController _controller;
-
-  @override
-  void initState() {
-    super.initState();
-    _controller = VideoPlayerController.asset('assets/wizard.mp4')
-      ..initialize().then((_) {
-        setState(() {
-          _controller.play();
-        });
-      });
-  }
-
-  @override
-  void dispose() {
-    super.dispose();
-    _controller.dispose();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('MasterPie Introduction', style: TextStyle(color: Colors.white),),
-        backgroundColor: PRIMARY_COLOR,
-        leading: InkWell(
-          onTap: () {
-            Navigator.pop(context);
-          },
-          child: const Icon(Icons.arrow_back_ios, color: Colors.white,),
-        ),
-      ),
-      body: Center(
-        child: _controller.value.isInitialized
-            ? AspectRatio(
-          aspectRatio: _controller.value.aspectRatio,
-          child: VideoPlayer(_controller),
-        )
-            : CircularProgressIndicator(),
-      ),
-      floatingActionButton: FloatingActionButton(
-        backgroundColor: DARK_PRIMARY_COLOR,
-        onPressed: () {
-          setState(() {
-            _controller.value.isPlaying
-                ? _controller.pause()
-                : _controller.play();
-          });
-        },
-        child: Icon(
-          _controller.value.isPlaying ? Icons.pause : Icons.play_arrow, color: Colors.white,
-        ),
-      ),
-    );
-  }
-}
+// import 'package:flutter/material.dart';
+// import 'package:video_player/video_player.dart';
+//
+// import '../color/app_colors.dart';
+//
+//
+// class VideoPlayerScreen extends StatefulWidget {
+//   const VideoPlayerScreen({super.key});
+//
+//   @override
+//   _VideoPlayerScreenState createState() => _VideoPlayerScreenState();
+// }
+//
+// class _VideoPlayerScreenState extends State<VideoPlayerScreen> {
+//   late VideoPlayerController _controller;
+//
+//   @override
+//   void initState() {
+//     super.initState();
+//     _controller = VideoPlayerController.asset('assets/wizard.mp4')
+//       ..initialize().then((_) {
+//         setState(() {
+//           _controller.play();
+//         });
+//       });
+//   }
+//
+//   @override
+//   void dispose() {
+//     super.dispose();
+//     _controller.dispose();
+//   }
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: const Text('MasterPie Introduction', style: TextStyle(color: Colors.white),),
+//         backgroundColor: PRIMARY_COLOR,
+//         leading: InkWell(
+//           onTap: () {
+//             Navigator.pop(context);
+//           },
+//           child: const Icon(Icons.arrow_back_ios, color: Colors.white,),
+//         ),
+//       ),
+//       body: Center(
+//         child: _controller.value.isInitialized
+//             ? AspectRatio(
+//           aspectRatio: _controller.value.aspectRatio,
+//           child: VideoPlayer(_controller),
+//         )
+//             : CircularProgressIndicator(),
+//       ),
+//       floatingActionButton: FloatingActionButton(
+//         backgroundColor: DARK_PRIMARY_COLOR,
+//         onPressed: () {
+//           setState(() {
+//             _controller.value.isPlaying
+//                 ? _controller.pause()
+//                 : _controller.play();
+//           });
+//         },
+//         child: Icon(
+//           _controller.value.isPlaying ? Icons.pause : Icons.play_arrow, color: Colors.white,
+//         ),
+//       ),
+//     );
+//   }
+// }
